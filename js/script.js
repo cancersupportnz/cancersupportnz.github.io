5de39b171d706fac87d2027b2a6aacce4f63456b
alanbaileyy <kiwirulescoding@gmail.com>
2025-07-23 12:06:00 +1200

    add translation and market event

--- js/script.js
+++ js/script.js
@@ -35,5 +35,21 @@ window.onload = function() {
 };
 
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
-  
+ function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
 storeScroll();
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
